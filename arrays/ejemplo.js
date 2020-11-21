96454b598bababb29394aff9d3518ba25c920b08
Emanuel E. Sosa <emanuelesausosa@gmail.com>
2020-11-20 20:20:20 -0600

    add arrays ejemplo y ejercicios

---
+++ arrays/ejemplo.js
@@ -0,0 +1,65 @@
+// declaracion
+// camelCase ->
+// lowerCamelCase -- variables, funciones, instancas
+// UpperCamelCase -- Clases, interfaces, typos, Enums
+// snake-case
+let myArray = [];
+let myArray2 = [1, 2, 2, 3, 85, 8, 8];
+let myArray3 = ["x", 2, {}, true]; // usemos un solo tipo, 'x' | "x"
+
+let texto = "Hola";
+let nombre = "Emanuel";
+texto = "Hola " + nombre + "Hola 2";
+texto = `Hola ${nombre}`; // template string --> backstick
+
+// lectura
+// 1. acceso directo (acceso a un elemento en especifico)
+console.log(myArray2[4]);
+// 2. todo el arreglo
+// 2.1 for
+for (let i = 0; i < myArray2.length; i++) {
+  console.log(myArray2[i]);
+}
+// 2.2 for each
+// con funcion flecha -> ES6
+myArray2.forEach((e) => {
+  console.log(e); // elemento o posicion ??
+});
+
+myArray2.forEach(
+  (e) => console.log(e) // elemento o posicion ??
+);
+
+// func normal
+myArray2.forEach(function (e) {
+  console.log(e);
+});
+// 2.3 map
+myArray2.map((t) => {
+  console.log(t);
+});
+
+// 2.4 for in
+for (let i in myArray2) {
+} // trae el indice
+
+// 2.5 for of
+for (let e of myArray2) {
+} // trae elemento (e) en concreto
+
+// 2.6 while
+let contador = 0;
+while (contador < myArray2.length) {
+  console.log("Algo ...", myArray2.length[contador]);
+  contador++;
+}
+// 2.7 funcion recursiva
+function recorrer(indice) {
+  if (indice > myArray2.length) return;
+  console.log(myArray2[indice]);
+  recorrer(indice++);
+}
+
+recorrer(myArray[0]);
+
+// insercion en caliente
---
+++ arrays/ejercicios-1.js
@@ -0,0 +1,8 @@
+// 1. dado un array de numeros naturales,
+// retornar un arreglo de la raíz cuadrada de los pares
+
+// 2. dado un array de numeros naturales,
+// retornar un arreglo de la potencia de los impares
+
+// 3. dada una lista de objetos par {nombre: "Lisa Simpson", edad: 11}
+// obtener una lista de las personas menores de edad,
